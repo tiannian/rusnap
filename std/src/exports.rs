e456f76275164f9b0318783e2d5cd6411f825a8e
tiannian <dtiannian@gmail.com>
2023-10-04 22:23:29 +0800

    add exports

---
+++ std/src/exports.rs
@@ -0,0 +1,35 @@
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::JsValue;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RpcRequest {
+    pub id: String,
+    pub jsonrpc: String,
+    pub method: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Request {
+    pub origin: String,
+    pub request: RpcRequest,
+}
+
+pub fn parse_rpc_request(req: JsValue) -> (String, RpcRequest) {
+    let req: Request = serde_wasm_bindgen::from_value(req).unwrap();
+
+    (req.origin, req.request)
+}
+
+#[macro_export]
+macro_rules! rpc_handler {
+    ($handler:ident) => {
+        #[wasm_bindgen::prelude::wasm_bindgen(js_name = onRpcRequest)]
+        pub async fn _on_rpc_request(req: wasm_bindgen::JsValue) -> wasm_bindgen::JsValue {
+            let (origin, request) = $crate::exports::parse_rpc_request(req);
+
+            let res = $handle(origin, request);
+
+            $crate::serde_wasm_bindgen::to_value(&res).unwrap()
+        }
+    };
+}
--- std/src/lib.rs
+++ std/src/lib.rs
@@ -2,3 +2,7 @@ pub mod snap;
 
 mod error;
 pub use error::*;
+
+pub mod exports;
+
+pub use serde_wasm_bindgen;
